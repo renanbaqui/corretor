de2a2577575f4baecfb639588dc59f317dfe22bb
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-05-13 11:32:30 -0300

    Add files via upload

--- corretor.py
+++ corretor.py
@@ -1,24 +1,42 @@
 from spellchecker import SpellChecker
+from nltk.tokenize import RegexpTokenizer
 
-with open('teste2.txt') as f:
-    lista=[word for line in f for word in line.split()]
+with open('relatos.txt', 'r', encoding='utf-8') as text_file:   #abre o arquivo com encoding utf-8
+    str = text_file.read()  #transforma o texto em string
+
+f = open("erros.txt", "w")
+
+#print(str)
+
+tokenizer = RegexpTokenizer(r'\w+')     #tokenizer do nlkt
+lista = tokenizer.tokenize(str)         #string tokenizado em lista
 
 #print(lista)
 
-unknown = flat_list
-spell = SpellChecker(language='pt')
+#unknown = flat_list
+spell = SpellChecker(language='pt')     #objeto do tipo SpellChecker em portugues
 
 # find those words that may be misspelled
-misspelled = spell.unknown(lista)
+misspelled = spell.unknown(lista)       #lista de palavras erradas 
 
 
-print (spell.known(misspelled)) # Returns those words that are in the word frequency list
-print (spell.unknown(misspelled)) # Returns those words that are not in the frequency list
+#print (spell.known(misspelled))    # Returns those words that are in the word frequency list
+#print (spell.unknown(misspelled))  # Returns those words that are not in the frequency list
 
 for word in misspelled:
     # Get the one `most likely` answer
-    print('palavra errada:')
+    f.write("palavra errada encontrada:")
+    f.write("\n")
+    f.write(word)
+    f.write("\n")    
+    f.write("sugestao de palavra correta:")
+    f.write("\n")
+    f.write(spell.correction(word))
+    f.write("\n")
+    
+    print('palavra errada encontrada:')
     print(word)
+    print('sugestao de palavra correta:')
     print(spell.correction(word))
 
     # Get a list of `likely` options
