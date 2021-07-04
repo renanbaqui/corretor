087b37bfda7c0a1cf6669dbb4ec7e0716601c869
Renan Baqui <46902597+renanbaqui@users.noreply.github.com>
2021-07-04 14:11:40 -0300

    Add files via upload

---
+++ repeticoes.py
@@ -0,0 +1,10 @@
+#nao esta funcionando corretamente, preserva duas linhas ao inves de uma
+
+linhas_vistas = set()#Conjuntos de linhas já vistas.
+arq_out = open("relatos_sem_repeticao.txt", "w", encoding='utf8') #Abre o arquivo de escrita
+
+for line in open("relatos_com_repeticao.txt", "r", encoding='utf8'):
+    if line not in linhas_vistas: #se a linha não está dentro do conjunto linhas_vistas
+        arq_out.write(line)
+        linhas_vistas.add(line) #O item de comparação se trata do conjunto linhas vistas
+arq_out.close()
