4ff4d50a2f575e6e3e2056b74717b1a9a911dc9f
Renan Baqui <46902597+renanbaqui@users.noreply.github.com>
2021-07-18 02:03:19 -0300

    Add files via upload

--- repeticoes.py
+++ repeticoes.py
@@ -1,6 +1,6 @@
 #nao esta funcionando corretamente, preserva duas linhas ao inves de uma
 
-linhas_vistas = set()#Conjuntos de linhas já vistas.
+linhas_vistas = set() #Conjuntos de linhas já vistas.
 arq_out = open("relatos_sem_repeticao.txt", "w", encoding='utf8') #Abre o arquivo de escrita
 
 for line in open("relatos_com_repeticao.txt", "r", encoding='utf8'):
